90089fb9c9f3d125e7f143d43c6f073c437db88b
acharlop <avicharlop@gmail.com>
2023-01-24 13:57:54 +0100

    write all data files

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import fs from "fs"
+import fs from 'fs'
 const API_KEY = process.env.SHEET_API_KEY
 const SHEET_ID = process.env.SHEET_ID
 
-const range = "Sheet1!A1:Z"
+const range = 'Sheet1!A1:Z'
 const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${range}`
 
 type RawData = Array<Array<string>>
@@ -10,6 +10,7 @@ interface Res {
   range: string
   values: RawData
 }
+type Questions = Record<number | string, string>
 
 let rawData: RawData
 
@@ -18,38 +19,72 @@ const getSheet = async () => {
     r.json()
   )
   rawData = data.values
+  rawData[0] = rawData[0].map((i) => i.toLowerCase())
 }
 
-const getSingleLanguageQuestions = (language: string) => {
-  const lang = language.toLowerCase()
-  const index = rawData[0].findIndex((i) => i.toLowerCase() === lang)
+const writeSingleLanguage = async (lang: string) => {
+  const index = rawData[0].findIndex((i) => i === lang)
   // validate language exists
   if (index < 0) throw Error(`${lang} not found`)
 
-  const questions: Record<number, string> = {}
+  const questions: Questions = {}
 
   for (let i = 2; i < rawData.length; ++i) {
     questions[parseInt(rawData[i][0])] = rawData[i][index]
   }
 
-  return questions
-}
-
-const writeSingleLanguage = async (language: string) => {
-  const data = getSingleLanguageQuestions(language)
-  await Bun.write(`./languages/${language}.json`, JSON.stringify(data, null, 2))
+  await writeJsonFile(lang, questions)
 }
 
 const writeAllSingleLanguages = async () => {
   await rawData[0].filter(Boolean).forEach(async (lang) => {
-    await writeSingleLanguage(lang.toLowerCase())
+    await writeSingleLanguage(lang)
   })
+}
+
+const writeAllLanguages = async () => {
+  const questions: Record<number, Questions> = {}
+
+  for (let q = 2; q < rawData.length; ++q) {
+    const id = parseInt(rawData[q][0])
+    questions[id] = {}
+    for (let l = 1; l < rawData[0].length; ++l) {
+      const lang = rawData[0][l]
+      questions[id][lang] = rawData[q][l]
+    }
+  }
+
+  await writeJsonFile('questions', questions)
+}
+
+const writeSupportedLanguages = async () => {
+  const languages: Record<string, string> = {}
+
+  for (let l = 1; l < rawData[0].length; ++l) {
+    languages[rawData[0][l]] = capitalize(rawData[1][l])
+  }
+
+  await writeJsonFile('languages', languages)
+}
+
+// utils
+const stringify = (data: object) => JSON.stringify(data, null, 2)
+
+const capitalize = (str: string) =>
+  str[0].toUpperCase() + str.slice(1).toLowerCase()
+
+const writeJsonFile = async (name: string, data: object) => {
+  const filePath = `data/${name}.json`
+  await Bun.write(filePath, stringify(data))
+  console.log('OK:', filePath)
 }
 
 const run = async () => {
   await getSheet()
 
   writeAllSingleLanguages()
+  writeAllLanguages()
+  writeSupportedLanguages()
 }
 
 run()
